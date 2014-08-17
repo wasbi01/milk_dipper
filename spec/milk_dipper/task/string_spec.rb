e4a09164413fcd37d3da1d6e273e4fb8475b8ae5
TakahiroKaneko <aim.tkaneko@gmail.com>
2014-08-17 22:51:50 +0900

    increase spec samples for estimate

--- spec/milk_dipper/task/string_spec.rb
+++ spec/milk_dipper/task/string_spec.rb
@@ -2,10 +2,11 @@ require "spec_helper"
 describe String do
   describe "estimate_time" do
     context "with minutes" do
-      subject{ "1.5hours" }
-      it "5400(1時間30分)に変換されること" do
-        expect(subject.estimated_time).to eq(5400)
+      def times
+        %w(1.5hours 1h30m 90min)
       end
+      subject{ times.map(&:estimated_time) }
+      it("全て5400sec(1時間半)に変換されること"){ should all eq(5400) }
     end
   end
 end
